5ceaf117b9ed07c0d92b18145f26796541e608e5
Nicole Li <h0s6@ugrad.cs.ubc.ca>
2018-04-29 17:30:52 -0700

    Added 10 guesses

--- battleship.py
+++ battleship.py
@@ -18,17 +18,25 @@ def random_col(board_in):
 # Hide the ship
 ship_row = random_row(board)
 ship_col = random_col(board)
+# print ship_row
+# print ship_col
 
-guess_row = int(raw_input("Guess Row: "))
-guess_col = int(raw_input("Guess Col: "))
-
-if guess_row == ship_row and guess_col == ship_col:
-  print "Congratulations! You sank my battleship!"
-else:
-    if guess_row not in range(5) or guess_col not in range(5):
-        "Oops, that's not even in the ocean."
-    elif board[guess_row][guess_col] == "X":
-        print "You guessed that one already."
+for turns in range(10):
+    if turns == 3:
+        print "Game Over"
     else:
-        print "You missed my battleship!"
-        board[guess_row][guess_col] = "X"
+        guess_row = int(raw_input("Guess Row: "))
+        guess_col = int(raw_input("Guess Col: "))
+
+        if guess_row == ship_row and guess_col == ship_col:
+          print "Congratulations! You sank my battleship!"
+          break
+        else:
+            if guess_row not in range(5) or guess_col not in range(5):
+                "Oops, that's not even in the ocean."
+            elif board[guess_row][guess_col] == "X":
+                print "You guessed that one already."
+            else:
+                print "You missed my battleship!"
+                board[guess_row][guess_col] = "X"
+            print "Turn", turns + 1
